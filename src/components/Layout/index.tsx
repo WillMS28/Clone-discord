5f5791c7c84ff0ec5194ebcff9a5004f2adc977b
WillMS28 <silva.miranda.william@gmail.com>
2021-04-11 20:00:31 -0300

    component ServerName

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import { Grid } from './styles'
 
 import ServerList from '../ServerList/index'
+import ServerName from '../ServerName/index'
 
 const Layout: React.FC = () => {
   return <Grid>
     <ServerList />
+    <ServerName />
   </Grid>
 }
 
