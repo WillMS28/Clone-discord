64343485bb5121beefc9c11105f1854b4afdf1c1
WillMS28 <silva.miranda.william@gmail.com>
2021-04-15 21:08:34 -0300

    Implement ChannelInfo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,11 +4,13 @@ import { Grid } from './styles'
 
 import ServerList from '../ServerList/index'
 import ServerName from '../ServerName/index'
+import ChannelInfo from '../ChannelInfo/index'
 
 const Layout: React.FC = () => {
   return <Grid>
     <ServerList />
     <ServerName />
+    <ChannelInfo />
   </Grid>
 }
 
