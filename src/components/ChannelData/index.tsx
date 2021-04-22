bfd1bbb1e1357f0acbf0e7284db2086540ca3ed4
WillMS28 <silva.miranda.william@gmail.com>
2021-04-21 22:09:04 -0300

    Imcrement ChannelMessage

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import ChannelMessage, { Mention } from '../ChannelMessage'
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
 
 const ChannelData: React.FC = () => {
   return (
     <Container> 
-      <Messages />
+      <Messages>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage 
+            key={n}
+            author='William Miranda'
+            date='21/04/2021'
+            content='Bem Vindo ao meu Canal!'
+          />
+        ))}
+
+
+
+        <ChannelMessage 
+          author='WMS'
+          date='21/04/2021'
+          content= {
+            <>
+              <Mention>@William Miranda</Mention>, Muito obrigado.
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #chat-livre"/>
